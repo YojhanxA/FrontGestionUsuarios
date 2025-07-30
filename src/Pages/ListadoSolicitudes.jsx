c319ac0f6cdcc2986078f812aa665d571dfb9ab4
Yojhan Arias Correa <125290541+YojhanxA@users.noreply.github.com>
2025-07-30 10:53:58 -0500

    Update ListadoSolicitudes.jsx

--- src/Pages/ListadoSolicitudes.jsx
+++ src/Pages/ListadoSolicitudes.jsx
@@ -9,8 +9,8 @@ const ListadoSolicitudes = () => {
     try {
       const response = await axios.get(
         mostrarPendientes
-          ?`${import.meta.env.VITE_API_URL}/api/solicitudes?estado=false`
-          :`${import.meta.env.VITE_API_URL}/api/solicitudes`;
+          ? `${import.meta.env.VITE_API_URL}/api/solicitudes?estado=false`
+          : `${import.meta.env.VITE_API_URL}/api/solicitudes`;
       );
       setSolicitudes(response.data);
     } catch (error) {
