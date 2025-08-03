5207b27878759e0a0951b9c32b704fde34443d0e
Yojhan Arias <zigzagflete@gmail.com>
2025-08-03 16:02:41 -0600

    Metricas con vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./css/Listar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Si no usas el import con `* as bootstrap` arriba.
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Si no usas el import con `* as bootstrap` arriba.
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Formulario from "./Pages/Formulario.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListadoSolicitudes from "./Pages/ListadoSolicitudes.jsx";
